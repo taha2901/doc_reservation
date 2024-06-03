9ace0fe535da9e9232686d7f60af3a92a614c62e
Taha Hamada <taha2901>
2024-06-03 22:01:09 +0300

    add routes , colors

---
+++ lib/core/theming/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorManagers{
+  static const Color mainBlue = Color(0xff247CFF);
+  static const Color gray = Color(0xffE5E5E5);
+
+}
---
+++ lib/features/on_boarding/on_board_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: 
+        Text('on Board',style: TextStyle(color: Colors.black),),
+      ),
+    );
+  }
+}
